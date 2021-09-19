3eeb55e6249c11f6036f87c059f42f6a1351360b
KwonGeneral <thtm9020@gmail.com>
2021-09-19 18:03:13 +0900

    0919 update

---
+++ settings/phantom_setting.py
@@ -0,0 +1,12 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# PhantomJS 드라이버 설정
+pha = 'C:/dev_files/pha/bin/pha.exe'
+driver = webdriver.PhantomJS(pha)
+
+# 크롤링할 사이트 호출
+driver.get("https://www.python.org/")
+
